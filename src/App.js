bbfebc60d1ead0e83d23b442f0f1810b87a41024
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-04-20 18:00:20 -0500

    Terminando de agregar/fixear estilos, componentes y agregando mas assets.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
+import PostList from './components/postList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <PostList />
     </div>
   );
 }
