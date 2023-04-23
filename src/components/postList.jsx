6727018e862df5f0c2fbf6a4a29d37be05c480f6
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-04-22 22:50:41 -0500

    Agregando efecto de carga para el filtrado de los post.

--- src/components/postList.jsx
+++ src/components/postList.jsx
@@ -1,24 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Post from './post';
 import getPosts from '../services/ThreePics';
 
 function PostList({ searchBy, setPosts, posts }) {
 
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
+        setLoading(true);
         getPosts(searchBy).then((posts) => {
             setPosts(posts);
+            setLoading(false);
         });
+
     }, [searchBy]);
 
-    return (
-        <div className='container p-4'>
-            {posts.map((post) => (
-                <Post key={post.id} createdAt={post.createdAt}
-                    autor={post.autor} text={post.text} comments={post.comments}
-                    image={post.image} />
-            ))}
-        </div>
-    );
+    if (loading) {
+        return (
+            <div className='container p-4'>
+                <p>Loading...</p>
+                <div className="spinner-border text-primary" role="status">                    
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className='container p-4'>
+                {posts.map((post) => (
+                    <Post key={post.id} createdAt={post.createdAt}
+                        autor={post.autor} text={post.text} comments={post.comments}
+                        image={post.image} />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default PostList;
